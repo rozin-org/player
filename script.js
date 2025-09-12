8a16d8e53aeef5f075641a04e0ba48ba4fe412d9
GR <10486725+gady-rozin@users.noreply.github.com>
2025-09-12 13:01:02 +0300

    fix typo

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let currentIndex = 0;
 let isShuffle = false;
 // ===========================================================
 // ✅ Service worker cache version
-const CACHE_NAME = 'play-it-now-v1.0.11'; // bump version
-cconst CURRENT_VERSION = '1.0.11';
+const CACHE_NAME = 'play-it-now-v1.0.13'; // bump version
+const CURRENT_VERSION = '1.0.13';
 // ===========================================================
 // ✅ Dexie setup
 const db = new Dexie('PlayItNowDB');
